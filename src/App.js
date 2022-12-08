77d795d2963595cc29bf34fe5faa89b34e8c07ed
glpitarch <104110115+glpitarch@users.noreply.github.com>
2022-12-08 16:01:51 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import NavBar from './components/NavBar.jsx';
+import ItemDetailContainer from './components/ItemDetailContainer.jsx';
+import ItemListContainer from './components/ItemListContainer.jsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+
+const App = () => {
+
+  return (
+    <>
+      <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route exact path='/' element={<ItemListContainer/>}/>
+        <Route path='/item/:id' element={<ItemDetailContainer/>}/>
+        <Route path='/category/:categoryId' element={<ItemListContainer/>}/>
+        <Route path='/category/:categoryId/:material' element={<ItemListContainer/>}/>
+        <Route path='/category/:categoryId/item/:id' element={<ItemDetailContainer/>}/>
+        <Route path='/category/:categoryId/:material/item/:id' element={<ItemDetailContainer/>}/>
+      </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
