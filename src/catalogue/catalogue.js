86bfa0e60b826d9f59ef291fb056b4438e96ac8d
glpitarch <104110115+glpitarch@users.noreply.github.com>
2023-01-06 15:34:09 -0300

    Delete src/catalogue directory

--- src/catalogue/catalogue.js
+++
@@ -1,9 +0,0 @@
-import catalogueRings from './catalogueRings.js'; 
-import catalogueEarrings from './catalogueEarrings.js';
-import catalogueChains from  './catalogueChains.js';
-import catalogueNecklaces from  './catalogueNecklaces.js';
-import cataloguePendants from './cataloguePendants.js';
-
-const catalogue = [...catalogueEarrings, ...catalogueRings, ...catalogueChains, ...catalogueNecklaces, ...cataloguePendants]
-        
-export default catalogue
